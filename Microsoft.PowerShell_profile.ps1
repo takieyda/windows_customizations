ad51c7c14d732421ad10f5715d66b02e8fa16438
Shohn Martin <takieyda@gmail.com>
2023-01-13 08:11:18 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,7 @@ Import-Module oh-my-posh
 Set-Theme PowerLevel10k-Classic
 
 New-Alias -Name 'll' -Value 'ls'
+
+Function ipa {
+    Get-NetIPAddress | Sort-Object -Property InterfaceIndex | Format-Table -Property InterfaceIndex, InterfaceAlias, IPAddress
+}
